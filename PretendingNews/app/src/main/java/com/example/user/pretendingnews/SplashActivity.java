f4386f44d8a2ec3c7ae13fc27a894c3bf1e66a07
Sumin Go <jennifer045020@gmail.com>
2017-11-04 21:45:55 +0900

    Start Projectgit add . XD

---
+++ PretendingNews/app/src/main/java/com/example/user/pretendingnews/SplashActivity.java
@@ -0,0 +1,28 @@
+package com.example.user.pretendingnews;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by user on 2017-11-04.
+ */
+
+public class SplashActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashActivity.this, MainActivity.class);
+                SplashActivity.this.startActivity(intent);
+                SplashActivity.this.finish();
+            }
+        },2500);
+    }
+}
