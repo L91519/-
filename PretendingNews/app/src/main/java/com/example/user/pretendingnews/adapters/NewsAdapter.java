d76556ae440f9b5af6b30f5d87ea3d9b43f39b4d
Sumin Go <jennifer045020@gmail.com>
2017-11-05 05:06:16 +0900

    ontouchlistener

--- PretendingNews/app/src/main/java/com/example/user/pretendingnews/adapters/NewsAdapter.java
+++ PretendingNews/app/src/main/java/com/example/user/pretendingnews/adapters/NewsAdapter.java
@@ -2,11 +2,14 @@ package com.example.user.pretendingnews.adapters;
 
 import android.annotation.SuppressLint;
 import android.content.Context;
+import android.graphics.PorterDuff;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 
 import com.bumptech.glide.Glide;
 import com.example.user.pretendingnews.R;
@@ -20,7 +23,7 @@ import java.util.ArrayList;
 /**
  * Created by user on 2017-11-05.
  */
-public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder > {
+public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder > implements View.OnTouchListener{
 
     private ArrayList<Object> items;
     private Context context;
@@ -53,12 +56,14 @@ public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder > 
             final TitleNewsHolder newsHolder = (TitleNewsHolder) holder;
             String tmp = ((NewsItem) items.get(position)).getMainImage();
             Glide.with(context).load(tmp).into(newsHolder.titleImage);
+            newsHolder.titleImage.setOnTouchListener(this);
         } else if (holder.getItemViewType() == NORMAL_TYPE) {
             final NormalNewsHollder normalNewsHolder = (NormalNewsHollder) holder;
             Glide.with(context).load(((NormalItem) items.get(position)).getFirstImage()).into(normalNewsHolder.firstNewsImage);
             Glide.with(context).load(((NormalItem) items.get(position)).getSecondImage()).into(normalNewsHolder.secondNewsImage);
             Log.d("--imageLog Check", ((NormalItem) items.get(position)).getSecondImage());
-
+            normalNewsHolder.secondNewsImage.setOnTouchListener(this);
+            normalNewsHolder.firstNewsImage.setOnTouchListener(this);
         }
     }
 
@@ -74,5 +79,30 @@ public class NewsAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder > 
             viewType = 0;
         }
         return viewType;
+    }
+
+    @Override
+    public boolean onTouch(View v, MotionEvent event) {
+
+        switch (event.getAction()) {
+            case MotionEvent.ACTION_DOWN: {
+                ImageView view = (ImageView) v;
+                //overlay is black with transparency of 0x77 (119)
+                view.getDrawable().setColorFilter(0x77000000,PorterDuff.Mode.SRC_ATOP);
+                view.invalidate();
+                break;
+            }
+            case MotionEvent.ACTION_UP:
+                v.performClick();
+            case MotionEvent.ACTION_CANCEL: {
+                ImageView view = (ImageView) v;
+                //clear the overlay
+                view.getDrawable().clearColorFilter();
+                view.invalidate();
+                break;
+            }
+        }
+
+        return true;
     }
 }
